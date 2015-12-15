5b6b76082b58703fafb5362b871570d7956087ad
Antoine Veriter <antoine.veriter@bea.be>
2015-12-15 11:38:30 +0100

    Added the core of the software: the code responsible for displaying "Hello World" :grin:

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int int main(void) {
+  cout << "Hello World!" << endl;
+  cout << "This program was written in C++" << endl;
+  cout << "Bye now!" << endl;
+
+  return 0;
+}
